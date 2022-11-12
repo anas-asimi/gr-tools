fa47449bf45b33c16ab74b94d39ee47b4e926e94
Asimi Anas <asimianas@outlook.com>
2022-11-12 13:48:15 +0000

    improve

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet, Link } from "react-router-dom";
 export default function Layout() {
 
   function menuToggle() {
-    let menuList= document.querySelector('.header-menu-list')
+    let menuList= document.querySelector('.header-menu-list-mobile')
     let menuButton = document.querySelector('.header-menuButton')
     menuList.classList.toggle('opened')
     menuButton.classList.toggle('opened')
@@ -14,12 +14,18 @@ export default function Layout() {
       <header>
         <div className="header-container">
           <Link to="/" className="header-brand">Genie Rural <span>tools</span></Link>
+          <ul className="header-menu-list-desktop">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/bassins">Bassins</Link></li>
+            <li><Link to="/pompage">Pompage</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
           <a className="header-menuButton" onClick={menuToggle}>
             <span></span>
             <span></span>
             <span></span>
           </a>
-          <ul className="header-menu-list">
+          <ul className="header-menu-list-mobile">
             <li><Link onClick={menuToggle} to="/">Home</Link></li>
             <li><Link onClick={menuToggle} to="/bassins">Bassins</Link></li>
             <li><Link onClick={menuToggle} to="/pompage">Pompage</Link></li>
