be5eedd07a83891f8c1b2f7f54e6389a0da9010f
anas-asimi <anas2020cr7@gmail.com>
2022-11-20 23:24:35 +0000

    improve

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const Notfound = () => {
     return (
         <main id="notFound">
-            <img src="/src/assets/error404.png" className="error-image"/>
+            <img src="./assets/error404.png" className="error-image"/>
             <p>Page not found.</p>
             <Link to="/">Go back to home</Link>
         </main>
