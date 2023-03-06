fa2f1d3ec08d8b288ee13c6ef5ba5767831f7613
Anas Asimi <anas2020cr7@gmail.com>
2023-03-07 00:54:53 +0100

    finish drive me feature

--- src/pages/DriveMe.jsx
+++ src/pages/DriveMe.jsx
@@ -1,7 +1,57 @@
+import transformation from 'transform-coordinates'
+import { useState } from "react";
+
+const zone_indexs = ['EPSG:26191', 'EPSG:26192', 'EPSG:26194', 'EPSG:26195']
+const WGS_index = 'EPSG:4326'
+
 export const DriveME = () => {
-  return (
-    <main id='drive-me'>
-      <h1>hello from Drive me page</h1>
-    </main>
-  )
+	const [parameters, setParameters] = useState({
+		x: '',
+		y: '',
+		zone: 1,
+	});
+
+	function handleChange(event) {
+		const name = event.target.name;
+		const value = parseInt(event.target.value);
+		setParameters({
+			...parameters,
+			[name]: value
+		})
+	}
+	function start() {
+		let transform = transformation(zone_indexs[parameters.zone - 1], WGS_index)
+		let {x,y} = transform.forward({x : parameters.x , y : parameters.y})
+		if (isNaN(x) && isNaN(y) ) {
+			const URL = `https://www.google.com/maps/search/${y},+${x}`
+			window.open(URL, '_blank');	
+		}
+	}
+	return (
+		<main id='drive-me'>
+			<div className="group">
+				<input type="number" name="x" value={parameters.x} onChange={handleChange} required />
+				<span className="highlight"></span>
+				<span className="bar"></span>
+				<label>X</label>
+			</div>
+			<div className="group">
+				<input type="number" name="y" value={parameters.y} onChange={handleChange} required />
+				<span className="highlight"></span>
+				<span className="bar"></span>
+				<label>Y</label>
+			</div>
+			<div className="group">
+				<select name="zone" value={parameters.zone} onChange={handleChange} required>
+					<option value="1">Lambert Zone I</option>
+					<option value="2">Lambert Zone II</option>
+					<option value="3">Lambert Zone III</option>
+					<option value="4">Lambert Zone IV</option>
+				</select>
+			</div>
+			<div className="group">
+				<button onClick={start}>Start</button>
+			</div>
+		</main>
+	)
 }
